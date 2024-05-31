627a553f99a1abf80bc08a013391826f584f1a53
sg13041995 <subhankar.github.130495@gmail.com>
2024-05-31 11:40:16 +0530

    pipline completed tested with apio

--- app.py
+++ app.py
@@ -10,34 +10,46 @@ text:str = "What is Text Summarization?"
 
 app = FastAPI()
 
+# Home page
 @app.get("/", tags=["authentication"])
 async def index():
+    # Redirects to "/docs"
+    # "/docs" is fast api ui
     return RedirectResponse(url="/docs")
 
 
-
+# Training route
 @app.get("/train")
 async def training():
     try:
         # os.system can run any command
         # to train the command is => python main.py
+        # Run the main.py file
         os.system("python main.py")
+
+        # If training was successful
         return Response("Training successful !!")
 
     except Exception as e:
+        # If training failed
         return Response(f"Error Occurred! {e}")
     
-
+# Prediction route
 @app.post("/predict")
 async def predict_route(text):
     try:
-
+        # Initializing prediction pipeline object
         obj = PredictionPipeline()
+
+        # Calling the predict method while passing the text to summarize
         text = obj.predict(text)
+
+        # Returning the summarized text
         return text
     except Exception as e:
         raise e
     
 
 if __name__=="__main__":
+    # Launching the server at localhost port 8080
     uvicorn.run(app, host="0.0.0.0", port=8080)
